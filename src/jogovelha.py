2ca541fdd78f006b0e253aa3fd2afea018b16c34
Luis Dielle <luisdielle@Luiss-MacBook-Pro.local>
2020-03-27 22:21:27 -0300

    Adding docs and src

---
+++ src/jogovelha.py
@@ -0,0 +1,11 @@
+def incializar():
+    tab = [(".") for i in range (3) for j in range(3)]
+    return tab
+
+
+def main():
+    jogo = inicializar()
+    print(jogo)
+if __name__ == "__main__":
+    main()
+    
